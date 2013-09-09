957d61a650c344f66fafb4a18c613e00ab1424a7
Kai Lieth <git@radiant-cove-7007>
2013-09-09 18:08:48 -0400

    ChooseYourWordsCarefully

---
+++ ChooseYourWordsCarefully/Program.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+
+public class Program {
+	public static void main(String[] args) {
+		Map<String, Integer> map = new HashMap<String,Integer>();
+		Scanner stdin = new Scanner(System.in);
+		while (stdin.hasNext()) {
+			Scanner line = new Scanner(stdin.nextLine());
+			while (line.hasNext()) {
+				String word = line.next().replaceAll("[,.;\'`\"()/:-]*","").toLowerCase();
+				if (map.containsKey(word)) {
+					map.put(word, map.get(word) + 1);
+				} else {
+					map.put(word,1);
+				}
+			}
+		}
+		int max = 0;
+		List<String> words = new ArrayList<String>();
+		for (String s : map.keySet()) {
+			int num = map.get(s);
+			if (num > max) {
+				words = new ArrayList<String>();
+				words.add(s);
+				max = num;
+			} else if (num == max)  {
+				words.add(s);
+			}
+		}
+
+		System.out.println(max + " occurrences");
+		for (String s : words) {
+			System.out.println(s);
+		}
+	}
+}
